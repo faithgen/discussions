e47eeda08b025956bc35453c13f63d351d6ea727
innoflash <innocentmazando@gmail.com>
2020-04-15 12:43:39 +0200

    added auth service provider

---
+++ src/Providers/AuthServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Faithgen\Discussions\Providers;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
